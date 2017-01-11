3b9d1b1baecb87fe97c8a9fccd47f87a5746d1f9
Eveliina <pevetiainen.ep@hotmail.com>
2017-01-11 22:24:04 +0200

    Add list of event signups

--- src/main/java/sec/project/controller/EventSignupController.java
+++ src/main/java/sec/project/controller/EventSignupController.java
@@ -2,6 +2,7 @@ package sec.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +13,16 @@ import sec.project.repository.EventSignupRepository;
 public class EventSignupController {
 
     @Autowired
-    private EventSignupRepository signupRepository;
+    private EventSignupRepository eventSignupRepository;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getRootPage() {
-        return "redirect:/eventsignups/new";
+        return "redirect:/eventsignups";
     }
 
     @RequestMapping(value = "/eventsignups", method = RequestMethod.GET)
-    public String getEventSignups() {
+    public String getEventSignups(Model model) {
+        model.addAttribute("eventsignups", eventSignupRepository.findAll());
         return "event_signups";
     }
 
@@ -31,7 +33,7 @@ public class EventSignupController {
 
     @RequestMapping(value = "/eventsignups", method = RequestMethod.POST)
     public String createEventSignup(@RequestParam String name, @RequestParam String address) {
-        signupRepository.save(new EventSignup(name, address));
+        eventSignupRepository.save(new EventSignup(name, address));
         return "redirect:/eventsignups?done";
     }
 
--- src/main/java/sec/project/repository/EventSignupRepository.java
+++ src/main/java/sec/project/repository/EventSignupRepository.java
@@ -3,6 +3,10 @@ package sec.project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sec.project.domain.EventSignup;
 
+import java.util.List;
+
 public interface EventSignupRepository extends JpaRepository<EventSignup, Long> {
+
+    List<EventSignup> findAll();
 
 }
